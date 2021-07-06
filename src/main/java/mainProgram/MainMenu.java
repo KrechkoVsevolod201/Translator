d2cb37bc4bd5cdc348db202fe6aa194b9cacec62
Sharky_001 <71152134+KrechkoVsevolod201@users.noreply.github.com>
2021-07-06 20:20:25 +0400

    Начало положенно

---
+++ src/main/java/mainProgram/MainMenu.java
@@ -0,0 +1,77 @@
+package mainProgram;
+import instruments.Sound;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URISyntaxException;
+
+public class MainMenu extends JFrame {
+
+    private final  JButton online = new JButton("Online");
+    private final  JButton offline = new JButton("Offline");
+
+    public MainMenu()
+    {
+        super("21");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // Определяем размер окна и выводим его на экран
+        setSize(450, 350);
+        setVisible(true);
+        try {
+            setContentPane(panel()); // передаем как параметр в коструктор
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        setVisible(true); //  форма будет видимой
+    }
+
+    public JPanel panel() throws URISyntaxException  {
+        // We create a bottom JPanel to place everything on.
+        // сначала создаётся "панель", на которой и размещаюся
+        // остальные компоненты
+        JPanel menushka = new JPanel(); // создаём "поверхность"
+        menushka.setLayout(null);
+
+        // Создаём кнопку---------------
+        online.setLocation(200, 100); // расположение кнопки
+        online.setSize(200, 40); // размер кнопки
+        online.setBackground(new Color(0x6DC911));
+        online.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Sound.playSound("sounds\\boyNextDoor.wav");
+
+            }
+        });
+
+
+        online.setActionCommand("Open");
+        menushka.add(online); // добавляем кнопку на поверхность
+
+        // Создаём кнопку---------------
+        offline.setLocation(200, 200); // расположение кнопки
+        offline.setSize(200, 40); // размер кнопки
+        offline.setBackground(new Color(0x157AA1));
+        offline.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        offline.setActionCommand("Open");
+        menushka.add(offline); // добавляем кнопку на поверхность
+
+        menushka.setOpaque(true);
+        //menushka.setBackground(Color.DARK_GRAY);
+        return menushka; // возвращаем внешний вид
+
+
+    }
+    public static void main(String[] args) {
+        new MainMenu();
+        Sound.playSound("sounds\\welcomeToTheClubBuddy.wav");
+    }
+}
